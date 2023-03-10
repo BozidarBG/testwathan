12d9786a31e420cfb5211a971131e1c978a6ae7f
Bozidar <bozidar.djordjevic@gmail.com>
2023-03-10 01:02:30 +0100

    drugi folder gotov

--- app/Http/Controllers/ConcertOrdersController.php
+++ app/Http/Controllers/ConcertOrdersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentFailedException;
 use App\Billing\PaymentGateway;
 use App\Models\Concert;
 use Illuminate\Http\Request;
@@ -19,15 +20,28 @@ class ConcertOrdersController extends Controller
     }
 
     public function store(Request $request, $concertId){
+        //nećemo validaciju ako ne postoji ovaj koncert
+        $concert=Concert::published()->findOrFail($concertId);
 
-        $concert=Concert::find($concertId);
+        $this->validate($request, [
+            'email'=>'required|email',
+            'ticket_quantity'=>'required|integer|min:1',
+            'payment_token'=>'required'
+        ]);
 
-        //charging customer
-        $this->payment_gateway->charge($request->ticket_quantity * $concert->ticket_price, $request->token);
+        try{
 
 
-        //create order
-        $order=$concert->orderTickets($request->email, $request->ticket_quantity);
-        return response()->json([], 201);
+            //charging customer
+            $this->payment_gateway->charge($request->ticket_quantity * $concert->ticket_price, $request->payment_token);
+
+
+            //create order
+            $order=$concert->orderTickets($request->email, $request->ticket_quantity);
+            return response()->json([], 201);
+        }catch(PaymentFailedException $e){
+            return response()->json([], 422);
+        }
+
     }
 }
--- tests/Unit/FakePaymentGatewayTest.php
+++ tests/Unit/FakePaymentGatewayTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Unit;
 
 use App\Billing\FakePaymentGateway;
+use App\Billing\PaymentFailedException;
 use PHPUnit\Framework\TestCase;
 
 class FakePaymentGatewayTest extends TestCase
@@ -15,5 +16,22 @@ class FakePaymentGatewayTest extends TestCase
         $payment_gateway->charge(2500, $payment_gateway->getValidToken());
 
         $this->assertEquals(2500, $payment_gateway->totalCharges());
+    }
+
+    //da očekujemo da bude exception
+    public function test_charges_with_invalid_payment_token_failed(){
+        try{
+            $payment_gateway=new FakePaymentGateway();
+
+            $payment_gateway->charge(2500, 'pogrešan-tonek');
+
+        }catch (PaymentFailedException $e){
+            return;
+        }
+
+        $this->fail();
+
+
+
     }
 }
